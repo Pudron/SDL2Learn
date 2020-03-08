3deb4fa1a9c0d7e29ec646d4670ec33f95082aef
Pudron <34957075+Pudron@users.noreply.github.com>
2020-03-08 21:00:49 +0800

    add files

---
+++ template/test.c
@@ -0,0 +1,51 @@
+#include<SDL2/SDL.h>
+#include<stdbool.h>
+bool isRun=true;
+SDL_Window*win=NULL;
+SDL_Renderer*ren=NULL;
+bool init(){
+	if(SDL_Init(SDL_INIT_VIDEO)<0){
+		return false;
+	}
+	win=SDL_CreateWindow("Test",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN);
+	if(win==NULL){
+		return false;
+	}
+	return true;
+}
+void close(){
+	if(ren!=NULL){
+		SDL_DestroyRenderer(ren);
+	}
+	if(win!=NULL){
+		SDL_DestroyWindow(win);
+	}
+	SDL_Quit();
+}
+void event(){
+	SDL_Event e;
+	if(SDL_PollEvent(&e)){
+		switch(e.type){
+			case SDL_QUIT:
+				isRun=false;
+				break;
+			default:
+				break;
+		}
+	}
+}
+void render(){
+	SDL_SetRenderDrawColor(ren,0xFF,0xFF,0xFF,0xFF);
+	SDL_RenderClear(ren);
+	SDL_RenderPresent(ren);
+}
+int main(int argc,char**argv){
+	if(!init()){
+		printf("SDL2 init error:%s\n",SDL_GetError());
+	}
+	while(isRun){
+		render();
+		event();
+	}
+	return 0;
+}
